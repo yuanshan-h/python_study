f01b3118cd667700168e4d999acfa661029e138d
yuanshan <2294349755@qq.com>
2022-05-10 21:44:01 +0800

    1

--- python_OOP/test.py
+++ python_OOP/test.py
@@ -1,1 +1,15 @@
-print("hello , huangchao")
+class Cat:
+    def __init__(self,name):
+        self.name=name
+    def eat(self):
+        print("%s爱吃鱼" % self.name)
+    def drink(self):
+        print("%s爱喝水" %self.name)
+    
+
+tom = Cat("tom")
+tom.eat()
+tom.drink() 
+print(tom.name)
+addr = id(tom)
+print("%x" %addr)
